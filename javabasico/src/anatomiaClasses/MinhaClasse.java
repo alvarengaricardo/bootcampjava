33573be0c0c0555ae2740a289957463b52be6cf5
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2024-05-13 22:24:32 -0300

    Anatomia das Classes 2/6

--- javabasico/src/anatomiaClasses/MinhaClasse.java
+++ javabasico/src/anatomiaClasses/MinhaClasse.java
@@ -3,6 +3,12 @@ package anatomiaClasses;
 public class MinhaClasse {
 
     public static void main(String [] args){
+
+        final String BR = "Brasil"; // Define uma constante BR de balor "Brasil"
+        int valor_$1 = 100;
+
         System.out.print("Primeira Classe!");
+
+
     }
 }
