b534d0c4474b64b90401b64a1ea43f367e42c19c
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2024-05-19 15:52:04 -0300

    Conclusão

---
+++ javabasico/src/edu/alvarenga/tiposVariaveis/TiposVariaveis.java
@@ -0,0 +1,10 @@
+package edu.alvarenga.tiposVariaveis;
+
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+      byte numero1 = 100;
+      short  numero2=numero1;
+      numero1 = (byte) numero2;
+      System.out.println(numero1);
+    }
+}
