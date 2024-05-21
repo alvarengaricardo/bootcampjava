ea82d901322e2f3d2c93d3b00d58582ac8d225c6
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2024-05-20 23:02:12 -0300

    Criação do Projeto

---
+++ ContaBanco/src/edu/alvarenga/contaBanco/ContaTerminal.java
@@ -0,0 +1,14 @@
+package edu.alvarenga.contaBanco;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        // TODO: importar a classe Scanner
+
+        // TODO: Exibir mensagens para o usuário
+
+        // TODO: Obter, via Scanner, os valores digitados no terminal
+
+        // TODO: Exibir a mensagem da conta criada
+
+    }
+}
