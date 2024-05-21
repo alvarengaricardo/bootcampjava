d067f3c3046df149c73fbbc520275fb4b438fd21
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2024-05-20 23:30:43 -0300

    Entradas de dados

--- ContaBanco/src/edu/alvarenga/contaBanco/ContaTerminal.java
+++ ContaBanco/src/edu/alvarenga/contaBanco/ContaTerminal.java
@@ -1,10 +1,30 @@
 package edu.alvarenga.contaBanco;
 
+import java.util.Scanner;
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        // TODO: importar a classe Scanner
 
-        // TODO: Exibir mensagens para o usuário
+    public static void main(String[] args) throws Exception {
+
+        String agencia;
+        Integer numero;
+        String nomeCliente;
+        Float saldo;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Por favor, digite o número da agência: ");
+        agencia = scanner.nextLine();
+
+        System.out.print("Por favor, digite o número da conta: ");
+        numero = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Por favor, digite nome do cliente: ");
+        nomeCliente = scanner.nextLine();
+
+        System.out.print("Por favor, digite o saldo inicial: ");
+        saldo = scanner.nextFloat();
+        scanner.nextLine();
+
 
         // TODO: Obter, via Scanner, os valores digitados no terminal
 
