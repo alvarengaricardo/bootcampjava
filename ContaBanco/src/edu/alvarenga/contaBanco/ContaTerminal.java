9f0b17bc4a208b5048ab728d0de124c7bff4f903
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2024-05-20 23:37:39 -0300

    Entradas exibição dos dados e conclusão.

--- ContaBanco/src/edu/alvarenga/contaBanco/ContaTerminal.java
+++ ContaBanco/src/edu/alvarenga/contaBanco/ContaTerminal.java
@@ -23,10 +23,9 @@ public class ContaTerminal {
         System.out.print("Por favor, digite o saldo inicial: ");
         saldo = Float.valueOf(scanner.nextLine());
 
-
-        // TODO: Obter, via Scanner, os valores digitados no terminal
-
-        // TODO: Exibir a mensagem da conta criada
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco," +
+                " sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo +
+                " já está disponível para saque.");
 
     }
 }
