5946a9237bec503946ebd1ce820ec84a93650771
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2024-05-21 16:36:56 -0300

    condicional composta

---
+++ javabasico/src/edu/alvarenga/condicional/CondicionalComposta.java
@@ -0,0 +1,12 @@
+package edu.alvarenga.condicional;
+
+public class CondicionalComposta {
+    // condicional composta
+    public static void main(String[] args) throws Exception {
+        float notaAluno = 7.0F;
+
+        if (notaAluno >= 7)
+            System.out.println("Aprovado!");
+        else
+            System.out.println("Reprovado.");
+}}
